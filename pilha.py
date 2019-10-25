e80b9016622a658c0da7723da6665ff2e019231b
lucashss88 <lucashss88@gmail.com>
2019-10-25 11:10:02 -0300

    class pilha

---
+++ pilha.py
@@ -0,0 +1,49 @@
+class No:
+    def __init__(self,dado=None,proximo=None):
+        self._dado=dado
+        self._proximo=proximo
+    def get_dado(self):
+        return self._dado
+    def set_dado(self,novoDado):
+        self._dado=novoDado
+    def get_proximo(self):
+        return self._proximo
+    def set_proximo(self,outro):
+        self._proximo=outro
+    def __str__(self):
+        return "{}".format(self._dado)
+class Pilha:
+    def __init__(self,head=None):
+        self._head=head
+    def isEmpty(self):
+        if self._head == None:
+            return True
+        else:
+            return False
+    def push(self,item):
+        p=self._head
+        q=No(item)
+        q.set_proximo(p)
+        q=self._head
+    def remove(self):
+        p=self._head
+        q=self._head
+        q.set_proximo(self._head)
+        p.set_proximo(None)
+    def size(self):
+        topo=self._itens[0]
+        cont=0
+        while(topo.get_proximo()!=None):
+            cont+=1
+            topo=topo.get_proximo()
+        return cont
+    def printall(self):
+        p=self._head
+        q=p
+        while(q.get_proximo()!=None):
+            q=q.get_proximo()
+        while(q!=p):
+            print(q)
+x= Pilha(1)
+x.push(7)
+x.push(2)
