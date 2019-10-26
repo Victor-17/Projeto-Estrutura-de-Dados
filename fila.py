c0f20a904831259581248cfe85b031c4ef34b826
Victor-17 <vejapyassu@gmail.com>
2019-10-25 22:08:57 -0300

    Adicionando a classe Fila.

---
+++ fila.py
@@ -0,0 +1,23 @@
+class Fila:
+    def __init__(self, head=None):
+        self._head = head
+    def isEmpty(self):
+        return self._head == None
+    def remove(self):
+        p = self._head
+        self._head = self._head.get_proximo()
+        return p.get_dado()
+    def add (self, no):
+        p = self._head
+        while(p.get_proximo != None):
+            p = p.get_proximo()
+        p.set_proximo(no)
+        return p
+    def size(self):
+        p = self._head
+        cont = 0
+        while(p.get_proximo() != None):
+            cont += 1
+            p = p.get_proximo()
+        cont += 1
+        return f'O tamanho da fila é de  itens..'
