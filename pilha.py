b8e9526fdc17ac04e251a4e9a44c1a8b6a46f4c5
lucashss88 <lucashss88@gmail.com>
2019-10-28 12:42:34 -0300

    Modificando pilha

--- pilha.py
+++ pilha.py
@@ -1,17 +1,3 @@
-class No:
-    def __init__(self,dado=None,proximo=None):
-        self._dado=dado
-        self._proximo=proximo
-    def get_dado(self):
-        return self._dado
-    def set_dado(self,novoDado):
-        self._dado=novoDado
-    def get_proximo(self):
-        return self._proximo
-    def set_proximo(self,outro):
-        self._proximo=outro
-    def __str__(self):
-        return "{}".format(self._dado)
 class Pilha:
     def __init__(self,head=None):
         self._head=head
