aab448dbaa222c2eeaeff91030a7abd0a81daa5c
Victor-17 <vejapyassu@gmail.com>
2019-10-29 11:11:12 -0300

    Adicionando novo método e corrigindo o erro do get_proximo()

--- fila.py
+++ fila.py
@@ -9,10 +9,10 @@ class Fila:
         return p.get_dado()
     def add (self, no):
         p = self._head
-        while(p.get_proximo != None):
+        while(p.get_proximo() != None):
             p = p.get_proximo()
         p.set_proximo(no)
-        return p
+        return p.get_proximo()
     def size(self):
         p = self._head
         cont = 0
@@ -20,4 +20,11 @@ class Fila:
             cont += 1
             p = p.get_proximo()
         cont += 1
-        return f'O tamanho da fila é de {cont} itens..'
+        return f'O tamanho da fila é de  itens..'
+    def printall(self):
+          p = self._head
+          print(p,"\n")
+          while(p.get_proximo() != None):
+              p = p.get_proximo()
+              print(p,"\n")
+          
